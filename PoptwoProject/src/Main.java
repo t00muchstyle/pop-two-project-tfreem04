4180389d1f38d4ead67c20d85c89603b0c21e9be
Thomas Freeman-Jones <tfreem04@dcs.bbk.ac.uk>
2019-03-18 20:13:47 +0000

    Add files via upload
    implemented GCD and one test case

---
+++ PoptwoProject/src/Main.java
@@ -0,0 +1,17 @@
+
+import fraction.Fraction;
+
+import fraction.FractionImpl;
+
+public class Main {
+
+    public static void main(String[] args) {
+// first test to see if the gcd is working and the new object is created.
+        FractionImpl Fraction = new FractionImpl(30,40);
+        System.out.println(Fraction.n);
+        System.out.println(Fraction.d);
+//  passed 
+
+    }
+}
+
